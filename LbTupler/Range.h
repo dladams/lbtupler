499e365cd960d245987fde75d81713f701ec2dc9
David Adams <dladams@bnl.gov>
2015-05-21 13:56:43 -0400

    Fix Range and expand test coverage.

--- LbTupler/Range.h
+++ LbTupler/Range.h
@@ -14,14 +14,24 @@
 template<class T>
 class Range {
 public:
+  class Iter {
+  public:
+    Iter(T val) : m_val(val) { };
+    T& operator*() { return m_val; }
+    Iter& operator++() { ++m_val; return *this; }
+    bool operator!=(const Iter rhs) { return this->m_val != rhs.m_val; }
+  private:
+    T m_val;
+  };
+public:
   Range(const T& val) : m_val1(val), m_val2(val) { };
   Range(const T& val1, const T& val2) : m_val1(val1), m_val2(val2) { };
   T& first() { return m_val1; }
   T& last() { return m_val2; }
   const T& first() const { return m_val1; }
   const T& last() const { return m_val2; }
-  const T& begin() const { return m_val1; }
-  const T& end() const { T val = m_val2; return ++val; }
+  Iter begin() const { return Iter(m_val1); }
+  Iter end() const { T val = m_val2; return Iter(++val); }
 private:
   T m_val1;
   T m_val2;
